0c9aae3843458f6a635a8718255c0ab78bd77ddb
Keith Bettinger <bettingr@stanford.edu>
2020-03-23 16:32:16 -0700

    Fixed bug in create_project where the name had quotes as part of it

--- gbsc-gcp_common.sh
+++ gbsc-gcp_common.sh
@@ -151,7 +151,7 @@ create_project() {
 
     # Create the project
     echo "Creating project $id named $name"
-    $DEBUG gcloud projects create $id --folder=$GCP_GBSC_FOLDER_ID --name="'$name'"
+    $DEBUG gcloud projects create $id --folder=$GCP_GBSC_FOLDER_ID "--name=$name"
     $DEBUG gcloud projects list --filter="project_id=$id"
 
     # Adding billing account.
